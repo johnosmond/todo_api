6ac40b6cc6bfc8ea07c32cbdfb5cc08c7b4ff957
John Osmond <john@jtech10.com>
2024-04-17 18:07:16 -0400

    Created first API endpoint

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TaskController;
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('/tasks', TaskController::class);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
